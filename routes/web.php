f1dfd9831e3e378a790595d1bebe2a43cf1a3d28
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2021-09-22 22:32:31 +0200

    Po poprawkach działająca na początek sekcji 10

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function ()
-{
-    return view('welcome');
-});
+Route::get('/', 'Home\MainPage')
+    ->name('home.mainPage');
 
 Route::get('users', 'UserController@list')
     ->name('get.users');
