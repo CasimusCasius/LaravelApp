c701790d1b81799757e96399a0515b4075cb004d
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2021-09-23 20:47:41 +0200

    database and migration

--- app/Http/Controllers/Home/MainPage.php
+++ app/Http/Controllers/Home/MainPage.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Home;
 
+
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class MainPage extends Controller
 {
     public function __invoke()
     {
+        $db = DB::connection();
+
+        dd($db);
+
+        $config = config('app.name');
+
         return view('home.main');
     }
 }
