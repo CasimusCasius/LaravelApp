4f77475163919bd113fa941be19afe2bcee54ce1
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2021-09-25 11:39:24 +0200

    seedery

--- app/Http/Controllers/Home/MainPage.php
+++ app/Http/Controllers/Home/MainPage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Home;
 
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
@@ -12,11 +13,6 @@ class MainPage extends Controller
 {
     public function __invoke()
     {
-        $db = DB::connection();
-
-        dd($db);
-
-        $config = config('app.name');
 
         return view('home.main');
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            GeneresSeeder::class,
+            GameSeeder::class
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
