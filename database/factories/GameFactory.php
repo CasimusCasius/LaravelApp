fd4ce098b65f9cff630fe49976404d48aef4c606
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2021-10-07 20:28:20 +0200

    fabryki i testy

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Game;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+$factory->define(Game::class, function (Faker $faker)
+{
+    return [];
+});
